88d98ca676c20ca3fbb9abb9b22b537ec69eb38f
“edusu” <edusuarez100@gmail.com>
2025-08-05 17:38:27 +0100

    fix

--- swap_estimator_rust/src/monitoring/client.rs
+++ swap_estimator_rust/src/monitoring/client.rs
@@ -7,7 +7,7 @@ use tokio::sync::{mpsc::Sender, oneshot};
 use crate::{
     error::{Error, EstimatorResult},
     monitoring::messages::MonitorRequest,
-    prices::{TokenId, TokenPrice},
+    prices::{TokenId, TokenPrice, estimating::OrderEstimationData},
 };
 
 #[derive(Debug, Clone)]
@@ -80,5 +80,34 @@ impl MonitorClient {
             .await
             .change_context(Error::ResponseError)
             .attach_printable("Failed to send result of remove check swap feasibility")
+    }
+
+    pub async fn estimate_orders_amount_out(
+        &self,
+        orders: Vec<OrderEstimationData>,
+    ) -> EstimatorResult<HashMap<String, u128>> {
+        let (resp_sender, resp_receiver) = oneshot::channel();
+        self.client
+            .send(MonitorRequest::EstimateOrdersAmountOut {
+                orders,
+                resp: resp_sender,
+            })
+            .await
+            .change_context(Error::ResponseError)
+            .attach_printable("Failed to send result of estimate orders amount out")?;
+        match resp_receiver.await {
+            Ok(Ok(data)) => Ok(data),
+            Ok(Err(e)) => {
+                tracing::error!("Error in monitoring service response: {e}");
+                Err(e.clone())
+                    .change_context(Error::ResponseError)
+                    .attach_printable_lazy(|| format!("Failed to estimate orders amount out: {e}"))
+            }
+            Err(_) => {
+                tracing::error!("Failed to receive response from monitoring service");
+                Err(report!(Error::ResponseError)
+                    .attach_printable("Failed to receive response from monitoring service"))
+            }
+        }
     }
 }
