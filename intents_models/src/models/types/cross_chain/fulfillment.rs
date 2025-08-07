1c31f7c4a4e9b4487eb906fa67bbd9244b84fef4
vadymykh <vadymykh@gmail.com>
2025-08-07 11:20:02 +0300

    fulfillment with external call

--- intents_models/src/models/types/cross_chain/fulfillment.rs
+++ intents_models/src/models/types/cross_chain/fulfillment.rs
@@ -1,10 +1,12 @@
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
-use crate::models::types::common::TransferDetails;
+use crate::models::types::common::{EvmCallMode, TransferDetails};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 /// Requested EVM fulfillment data
 pub struct EvmCrossChainFulfillmentData {
+    /// Destination chain guard address
+    pub dest_chain_guard_address: String,
     /// Requested fulfillment data
     pub requested_fulfillment: EvmCrossChainRequestedFulfillment,
     /// Auctioneer signature used to fulfill order on destination chain
@@ -15,7 +17,7 @@ pub struct EvmCrossChainFulfillmentData {
 /// Requested EVM fulfillment data
 pub enum EvmCrossChainRequestedFulfillment {
     SimpleFulfillment(SimpleEvmRequestedFulfillment),
-    // FulfillmentWithExternalCall(), // todo
+    FulfillmentWithExternalCall(EvmRequestedFulfillmentWithExternalCall),
 }
 
 #[serde_as]
@@ -34,6 +36,42 @@ pub struct SimpleEvmRequestedFulfillment {
     /// Main token amount
     #[serde_as(as = "DisplayFromStr")]
     pub requested_amount: u128,
+
+    /// Array of requested extra transfers
+    pub extra_transfers: Vec<TransferDetails>,
+}
+
+#[serde_as]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+/// Requested fulfillment data (with external call)
+pub struct EvmRequestedFulfillmentWithExternalCall {
+    /// Order ID
+    pub order_id: String,
+    /// Fulfillment deadline, in seconds
+    pub deadline: u64,
+    /// Main token address. address(0) for native token
+    pub token: String,
+    /// Main token destination address
+    /// For `CallMode.ApproveAndCall` tokens are approved to this address
+    /// For `CallMode.TransferAndCall` tokens are transferred to this address
+    pub token_destination: String,
+    /// Main token amount
+    /// For `CallMode.ApproveAndCall` this is minimum approval amount
+    /// For `CallMode.TransferAndCall` this is minimum transfer amount
+    #[serde_as(as = "DisplayFromStr")]
+    pub requested_amount: u128,
+
+    /// Contract address that must be called
+    pub call_target: String,
+    /// Call data of requested call to `callTarget`
+    pub call_data: String,
+    /// Requested call mode
+    pub call_mode: EvmCallMode,
+
+    /// In case of failed call, tokens will be sent to `fallbackAddress`
+    /// All remaining main tokens and tokens from extra transfers will also be sent to `fallbackAddress`
+    pub fallback_address: String,
 
     /// Array of requested extra transfers
     pub extra_transfers: Vec<TransferDetails>,
