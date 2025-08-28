3ceb924d8e5cd8e6973ffa8233275d79cad2696a
vadymykh <vadymykh@gmail.com>
2025-08-28 14:03:49 +0300

    SingleChainStartOrderSuiData

--- intents_models/src/models/types/single_chain/common/solver_types.rs
+++ intents_models/src/models/types/single_chain/common/solver_types.rs
@@ -33,6 +33,8 @@ pub enum SingleChainSolverStartOrderData {
     EVM(StartOrderEVMData),
     /// Solana-based chain data
     Solana(StartOrderSolanaData),
+    /// Sui-based chain data
+    Sui(SingleChainStartOrderSuiData),
 }
 
 impl SingleChainSolverStartOrderData {
@@ -69,4 +71,24 @@ pub struct SingleChainOrderExecutionDetails {
     pub chain_id: ChainId,
     pub intent_id: String,
     pub tx_hash: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+/// Sui-specific chain data, used by Solver to start single chain order execution
+pub struct SingleChainStartOrderSuiData {
+    /// Package ID, that should be interacted with
+    pub package_id: String,
+    /// Guard object address
+    pub guard_id: String,
+    /// Order object address
+    pub order_id: String,
+    /// Signer permission to start the order
+    pub signed_permission_data: Vec<u8>,
+    /// Auctioneer permission signature
+    pub auctioneer_signature: Vec<u8>,
+    /// Protocol fee amount that should be passed to function call
+    pub protocol_fee_amount: u64,
+    /// Type arguments for the function call
+    /// `FeeToken, StableCoin, CoinIn, CoinOut, ExtraTransferCoinOut`
+    pub types: [String; 5],
 }
