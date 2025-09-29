14a012178e75201e7a9b4c0a504e53babb704edb
“edusu” <edusuarez100@gmail.com>
2025-09-29 15:55:48 +0200

    feat: Updating log function

--- intents_models/src/log.rs
+++ intents_models/src/log.rs
@@ -1,9 +1,16 @@
 use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt as _};
 
-pub fn init_tracing() {
-    tracing_subscriber::registry()
-        .with(EnvFilter::from_default_env())
-        // TODO: Create env var to check if in prod or develop and use different formats
-        .with(fmt::layer().json().pretty().with_ansi(true))
-        .init();
+pub fn init_tracing(prod_format: bool) {
+    if prod_format {
+        tracing_subscriber::registry()
+            .with(EnvFilter::from_default_env())
+            .with(fmt::layer().json().flatten_event(true).with_ansi(false))
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            .with(EnvFilter::from_default_env())
+            // TODO: Create env var to check if in prod or develop and use different formats
+            .with(fmt::layer().json().pretty().with_ansi(true))
+            .init();
+    }
 }
