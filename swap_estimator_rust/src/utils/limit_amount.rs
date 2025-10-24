2e6ee48cf4dfad3ad4348484a34d4c458936025e
“edusu” <edusuarez100@gmail.com>
2025-10-24 15:21:16 +0200

    fix: fixing slippage % calculation to never get less amount limit than we want

--- swap_estimator_rust/src/utils/limit_amount.rs
+++ swap_estimator_rust/src/utils/limit_amount.rs
@@ -104,7 +104,8 @@ fn compute_limit_with_scaled_percentage(
         }
     }
 
-    Err(report!(Error::ParseError).attach_printable("Unable to compute limit amount without overflow"))
+    Err(report!(Error::ParseError)
+        .attach_printable("Unable to compute limit amount without overflow"))
 }
 
 #[inline]
@@ -131,11 +132,11 @@ pub fn get_slippage_percentage(
 
     let raw_pct = match trade_type {
         TradeType::ExactIn => {
-            // (estimated - limit) / estimated * 100
-            (est - lim) / est * Decimal::from(100u32)
+            // (estimated - limit) / estimated * 100 (almost)
+            (est - lim) / est * Decimal::from(999u32) / Decimal::from(10u32)
         }
         TradeType::ExactOut => {
-            // (limit - estimated) / estimated * 100
+            // (limit - estimated) / estimated * 100 (almost)
             (lim - est) / est * Decimal::from(100u32)
         }
     };
@@ -174,5 +175,37 @@ mod tests {
         let limit_amount = get_limit_amount_u64(TradeType::ExactOut, 1000, Slippage::Percent(2.0))
             .expect("Failed to get limit amount");
         assert_eq!(limit_amount, 1020);
+    }
+
+    #[test]
+    fn test_get_slippage_percentage() {
+        let amount_estimated = 12345678909876543210;
+        let amount_limit = 10345678901234567890;
+
+        let mut count = 0;
+        for i in 0..100 {
+            for j in 1..10 {
+                let test_limit_amount = (amount_limit + i) / j;
+                let slippage = get_slippage_percentage(
+                    amount_estimated,
+                    test_limit_amount,
+                    TradeType::ExactIn,
+                )
+                .unwrap();
+
+                let calculated_limit_amount = compute_limit_with_scaled_percentage(
+                    amount_estimated,
+                    slippage,
+                    TradeType::ExactIn,
+                )
+                .expect("Failed to get limit amount");
+
+                if calculated_limit_amount < test_limit_amount {
+                    count += 1;
+                }
+                // assert!(calculated_limit_amount >= test_limit_amount);
+            }
+        }
+        assert_eq!(count, 0);
     }
 }
